063a1c3479fe6bf78637563302f8f03366bf3a7f
Dinesh Sundaram <dinaswm2@gmail.com>
2022-03-31 16:05:30 +0530

    secret-key setting changes

--- LeFarm/settings.py
+++ LeFarm/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'l-fvs-md!(j-d#be!+-@vp=@q)&7k*is@og_dl0dhw*=ev)d_k'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
